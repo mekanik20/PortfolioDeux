ca47d0fba5ddd3aef2c41f57e32061e44b37b84b
Mike H <72580577+mekanik20@users.noreply.github.com>
2022-10-28 15:09:48 -0400

    Adding hamburger menu
    Added a hamburger menu for mobile viewing

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,30 @@
 /**
+ * Shows / hides burger menu when user clicks on burger or link
+ */
+(function () {
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menu");
+
+  // When the user clicks on the hamburger, toggle active class to show/hide burger menu
+  hamburger.addEventListener("click", mobileMenu);
+
+  function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  }
+
+  // When the user clicks on a link in the menu, remove active class to hide burger menu
+  const navLink = document.querySelectorAll(".nav-link");
+
+  navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+  function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }
+})();
+
+/**
  * Shows header when user scrolls up, hides header when user scrolls down */
 (function () {
 
